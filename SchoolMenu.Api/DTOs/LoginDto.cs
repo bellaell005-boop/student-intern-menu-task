b7bf94a09273a3bf4a11d9365b6431b593e3fd73
bellaell005-boop <bella.ell005@gmail.com>
2026-07-08 21:42:30 +0300

    Add Email property to LoginDto class

--- SchoolMenu.Api/DTOs/LoginDto.cs
+++ SchoolMenu.Api/DTOs/LoginDto.cs
@@ -9,6 +9,6 @@ namespace SchoolMenu.Api.DTOs;
 // ============================================================
 public class LoginDto
 {
-    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
     public string Password { get; set; } = "";
 }
